cdbad742c2df8ff4d6d0ac8b6ffde9b6921a3b92
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-10-26 12:25:55 -0400

    Add development containers for PKI @ JDK11
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+buildah bud -f devel/fedora_28 --tag cipherboy_fedora:28 .
+buildah bud -f devel/fedora_rawhide --tag cipherboy_fedora:rawhide .
+buildah bud -f devel/fedora_jdk11 --tag cipherboy_fedora:jdk11 .
+buildah bud -f devel/pki_jdk11 --tag cipherboy_pki:jdk11 .
