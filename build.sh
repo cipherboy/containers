c10955ab674a0c663ace08f9f2dd8b8c41fb20fa
Alexander Scheel <ascheel@redhat.com>
2018-10-29 15:32:49 -0400

    Update pki:jdk11
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 buildah bud -f devel/fedora_28 --tag cipherboy_fedora:28 .
 buildah bud -f devel/fedora_rawhide --tag cipherboy_fedora:rawhide .
 buildah bud -f devel/fedora_jdk11 --tag cipherboy_fedora:jdk11 .
