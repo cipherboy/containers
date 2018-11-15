0d350f2e7eed00a838194cbd6296b5d7874aa4fb
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-11-15 16:05:49 -0500

    Add new container images, update others
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -e
 
 buildah bud -f devel/fedora_28 --tag cipherboy_fedora:28 .
 buildah bud -f devel/fedora_rawhide --tag cipherboy_fedora:rawhide .
+buildah bud -f devel/ubuntu_1804 --tag cipherboy_ubuntu:bionic .
+buildah bud -f devel/debian_testing --tag cipherboy_debian:testing .
 buildah bud -f devel/fedora_jdk11 --tag cipherboy_fedora:jdk11 .
 buildah bud -f devel/pki_jdk11 --tag cipherboy_pki:jdk11 .
+buildah bud -f devel/jss_jdk11 --tag cipherboy_jss:jdk11 .
