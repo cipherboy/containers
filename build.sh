8e5dcd123f38d54bedbc7de3d36b4c6e1079fc2a
Alexander Scheel <ascheel@redhat.com>
2018-11-16 13:43:27 -0500

    Updates to ubuntu 18.04 image, add Fedora 29 image
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -e
 
 buildah bud -f devel/fedora_28 --tag cipherboy_fedora:28 .
+buildah bud -f devel/fedora_29 --tag cipherboy_fedora:29 .
 buildah bud -f devel/fedora_rawhide --tag cipherboy_fedora:rawhide .
 buildah bud -f devel/ubuntu_1804 --tag cipherboy_ubuntu:bionic .
 buildah bud -f devel/debian_testing --tag cipherboy_debian:testing .
